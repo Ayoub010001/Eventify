c39d678a3b9694914ada905ea372eb733750b315
Ayoub010001 <mrayoubr55@gmail.com>
2024-09-03 13:29:34 +0100

    SecurityFilterChain & h2db authorization

--- src/main/java/net/ayoub/eventify/security/SecurityConfig.java
+++ src/main/java/net/ayoub/eventify/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.
+                csrf(AbstractHttpConfigurer::disable).
                 formLogin(Customizer.withDefaults()).
                 authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/inscription").permitAll()
                         .anyRequest().authenticated())
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin())
+                )
                 .build();
     }
 }
